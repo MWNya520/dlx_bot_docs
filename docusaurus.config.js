f44be4e7bf309965c8c7bd54e3acd095722ef8ea
过客是个铁憨憨 <101338461+1811304592@users.noreply.github.com>
2024-08-11 22:59:07 +0800

    abab

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,6 +118,10 @@ const config = {
                 label: 'GitHub',
                 href: 'https://github.com/shengwang52005',
               },
+              {
+                label: '服务状态看板',
+                href: 'https://status.miaowing.top/status/dlxbot',
+              },
             ],
           },
         ],
